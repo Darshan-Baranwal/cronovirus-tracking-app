78d3b1235fdcdc6e1abfa4f39a95334398612300
Darshan Baranwal <Darshan.Baranwal@deutschebahn.com>
2020-04-30 20:54:23 +0200

    server js changes

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+const path = require('path');
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
